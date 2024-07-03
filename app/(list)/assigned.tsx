2a3a4df244f4a02c01c9e114dd73a788ccba8f27
iheblafi <iheb.lafi@esprit.tn>
2024-07-03 19:38:28 +0200

    trucking 101

--- app/(list)/assigned.tsx
+++ app/(list)/assigned.tsx
@@ -1,3 +1,5 @@
+//@ts-nocheck
+
 import React, { useEffect, useState } from 'react';
 import {
   View,
@@ -16,7 +18,8 @@ import CONFIG from '../config/config';
 import { useAuth } from '@/context/auth';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as Location from 'expo-location';
-import MapView, { Marker } from 'react-native-maps';
+import MapView, { Callout, Marker } from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
 interface User {
   _id: string;
   username: string;
@@ -37,11 +40,13 @@ interface Command {
   productId: Product;
   quantity: number;
   status: string;
+  longitude: number;
+  latitude: number;
 }
 
 const AssignedCommands = () => {
-  const { state:stateAuth } = useAuth();
-  
+  const { state: stateAuth } = useAuth();
+
   const [commands, setCommands] = useState<Command[]>([]);
   const [loading, setLoading] = useState(true);
   const [selectedCommand, setSelectedCommand] = useState<Command | null>(null);
@@ -49,81 +54,100 @@ const AssignedCommands = () => {
   const [newStatus, setNewStatus] = useState<string>('');
 
   const [state, setState] = useState({
-    latitude:0,longitude:0,error:null,routeCoordinates:[],distanceTravelled:0,valueprevLatlng:{}
+    latitude: 0, longitude: 0, error: null, routeCoordinates: [], distanceTravelled: 0, valueprevLatlng: {}
   })
 
-  const [initialRegion, setInitialRegion] = useState(null);
-  const [region,setRegion] = useState(null)
+  const [showDetails, setShowDetails] = useState(false);
 
-// get initial position
+  const toggleShowDetails = () => {
+    setShowDetails((state) => !state);
+  }
+
+  const [initialRegion, setInitialRegion] = useState(null);
+  const [region, setRegion] = useState(null)
+
+  // get initial position
   useEffect(() => {
-   const getCurrentPosition= async () => {
-    const { coords:{ latitude, longitude } } = await Location.getCurrentPositionAsync();
-    setInitialRegion({
-      latitude,
-      longitude,
-      latitudeDelta: 0.01,
-      longitudeDelta: 0.01,
-    });
-    setState(s => {return {...s,latitude:latitude,longitude:longitude}});
+    const getCurrentPosition = async () => {
+      const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync();
+      setInitialRegion({
+        latitude,
+        longitude,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      });
+      setState(s => { return { ...s, latitude: latitude, longitude: longitude } });
     }
 
-   const askPermission = async () => {
-    const permission = await Location.requestForegroundPermissionsAsync();
-    return permission.status === 'granted';
-  };
- 
-  const checkPermission = async () => {
-    const hasPermission = await Location.requestForegroundPermissionsAsync();
-    if (hasPermission.status === 'granted') {
+    const askPermission = async () => {
+      const permission = await Location.requestForegroundPermissionsAsync();
+      return permission.status === 'granted';
+    };
+
+    const checkPermission = async () => {
+      const hasPermission = await Location.requestForegroundPermissionsAsync();
+      if (hasPermission.status === 'granted') {
         const permission = await askPermission();
         return permission;
-    }
-    return true;
-};
+      }
+      return true;
+    };
 
-  checkPermission();
+    checkPermission();
 
     getCurrentPosition();
     return () => {
-      
+
     }
   }, [])
 
   // update position
   useEffect(() => {
-    (async ()=>{
-      let {status} = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted'){
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
         return;
       }
       let location = await Location.getCurrentPositionAsync();
-      setState(state => {return{...state,latitude:location.coords.latitude,longitude:location.coords.longitude}});
+      setState(state => { return { ...state, latitude: location.coords.latitude, longitude: location.coords.longitude } });
 
       let locations = await Location.watchPositionAsync({
-        accuracy:Location.Accuracy.High,
-        distanceInterval:2,
-        timeInterval:3000
-      },(location)=>{
-        setState((state)=> {return {...state,
-          // routeCoordinates:state.routeCoordinates.concat([{latitude:location.coords.latitude,longitude:location.coords.longitude}]),
-          latitude:location.coords.latitude,longitude:location.coords.longitude}});
+        accuracy: Location.Accuracy.High,
+        distanceInterval: 2,
+        timeInterval: 3000
+      }, (location) => {
+        setState((state) => {
+          return {
+            ...state,
+            // routeCoordinates:state.routeCoordinates.concat([{latitude:location.coords.latitude,longitude:location.coords.longitude}]),
+            latitude: location.coords.latitude, longitude: location.coords.longitude
+          }
+        });
       })
-      
+
     })();
-  
+
     return () => {
-      
+
     }
   }, [newStatus])
-  
 
-  const getMapRegion=()=>{
+
+  const getMapRegion = () => {
     return {
-      latitude:state.latitude,
-      longitude:state.longitude,
-      latitudeDelta:0.009,
-      longitudeDelta:0.009
+      latitude: state.latitude,
+      longitude: state.longitude,
+      latitudeDelta: 0.009,
+      longitudeDelta: 0.009
+    }
+  }
+
+  const getDestination = () => {
+    return {
+      latitude: selectedCommand.latitude + 0.0001,
+      longitude: selectedCommand.longitude + 0.0001,
+      latitudeDelta: 0.009,
+      longitudeDelta: 0.009
     }
   }
 
@@ -135,26 +159,28 @@ const AssignedCommands = () => {
         longitude: state.longitude,
       });
     }
-    (async() =>{
-      try {
-        const token = await AsyncStorage.getItem('token');
-        if (!token) {
-          throw new Error('No token found');
+    if (selectedCommand?.status === 'picked') {
+      (async () => {
+        try {
+          const token = await AsyncStorage.getItem('token');
+          if (!token) {
+            throw new Error('No token found');
+          }
+          await axios.post(`${CONFIG.API_BASE_URL}/api/delivery/auth/update-location`, {
+            longitude: state?.longitude,
+            latitude: state?.latitude,
+            commandId: selectedCommand?._id
+          }, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+        } catch (error) {
+          console.error('Error creating command:', error);
         }
-        await axios.post( `${CONFIG.API_BASE_URL}/api/delivery/auth/update-location`, {
-          longitude:state?.longitude,
-          latitude:state?.latitude,
-          commandId:selectedCommand?._id
-        }, {
-          headers: {
-            Authorization: `Bearer ${stateAuth.token}`,
-          },
-        });
-      } catch (error) {
-        console.error('Error creating command:', error);
-      }
-    })();
-  }, [state]);
+      })();
+    }
+  }, [state, newStatus]);
 
   useEffect(() => {
     const fetchAssignedCommands = async () => {
@@ -193,7 +219,7 @@ const AssignedCommands = () => {
           }
         );
         console.log('Status update response:', response.data);
-        setCommands(commands.map(command => 
+        setCommands(commands.map(command =>
           command._id === selectedCommand._id ? { ...command, status: newStatus } : command
         ));
         setModalVisible(false);
@@ -206,8 +232,11 @@ const AssignedCommands = () => {
   };
 
 
+  useEffect(() => {
+    if (newStatus === 'completed')
+      updateCommandStatus();
+  }, [newStatus])
 
-  
 
 
   return (
@@ -220,16 +249,16 @@ const AssignedCommands = () => {
             <Text>Product: {item.productId.name}</Text>
             <Text>Quantity: {item.quantity}</Text>
             <Text>Status: {item.status}</Text>
-            <TouchableOpacity onPress={() => { setSelectedCommand(item); setModalVisible(true); }}>
+            {item.status != 'completed' && <TouchableOpacity onPress={() => { setSelectedCommand(item); setModalVisible(true); }}>
               <Text style={styles.changeStatusText}>Change status</Text>
-            </TouchableOpacity>
-             </View>
+            </TouchableOpacity>}
+          </View>
 
         )}
         keyExtractor={(item) => item._id}
       />
 
-{selectedCommand && (
+      {selectedCommand && (
         <Modal
           transparent={true}
           visible={modalVisible}
@@ -251,22 +280,99 @@ const AssignedCommands = () => {
               <Button title="Cancel" onPress={() => setModalVisible(false)} />
             </View>
           </View>
-          <MapView key={"map"}  style={styles.map} region={
-        region
-      }
-      onRegionChangeComplete={(newRegion) => setInitialRegion({
-        ...initialRegion,
-        latitudeDelta: newRegion.latitudeDelta,
-        longitudeDelta: newRegion.longitudeDelta,
-      })}
+          {selectedCommand?.status === 'picked' &&
+            <View>
 
-      >
-        <Marker coordinate={getMapRegion()}>
-          <Image source={require("../../assets/icons/truck.png")} style={{width:30,height:30}}/>
-          </Marker>
-      </MapView>
+              <MapView key={"map"} style={styles.map} region={
+                region
+              }
+                onRegionChangeComplete={(newRegion) => setInitialRegion({
+                  ...initialRegion,
+                  latitudeDelta: newRegion.latitudeDelta,
+                  longitudeDelta: newRegion.longitudeDelta,
+                })}
+
+              >
+                <Marker coordinate={getMapRegion()}>
+                  <Image source={require("../../assets/icons/truck.png")} style={{ width: 30, height: 30 }} />
+                </Marker>
+                <Marker coordinate={getDestination()}>
+                  {/* <MapViewDirections
+    origin={getMapRegion()}
+    destination={getDestination()}
+    apikey={""}
+  /> */}
+                </Marker>
+
+              </MapView>
+              <View style={{ display: "flex", direction: "row" }}>
+                <TouchableOpacity
+                  onPress={() => { toggleShowDetails() }}
+                  style={{
+                    flex: 1,
+                    flexDirection: 'row',
+                    end: "22%",
+                    position: 'absolute',
+                    bottom: 10,
+                    alignSelf: "flex-end",
+                    justifyContent: "space-between",
+                    backgroundColor: "white",
+                    color: "white",
+                    borderWidth: 0.5,
+                    paddingVertical: 20,
+                    paddingHorizontal: 38,
+                    borderRadius: 20
+                  }}>
+                  {showDetails ? <Image source={require("../../assets/icons/eye-hide.png")} style={{ width: 30, height: 20 }}></Image> : <Image source={require("../../assets/icons/eye.png")} style={{ width: 30, height: 20 }}></Image>}
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                  onPress={() => { setNewStatus('completed') }}
+                  style={{
+                    flex: 1,
+                    flexDirection: 'row',
+                    position: 'absolute',
+                    bottom: 10,
+                    start: "22%",
+                    alignSelf: "flex-start",
+                    justifyContent: "space-between",
+                    backgroundColor: "green",
+                    color: "white",
+                    borderWidth: 0.5,
+                    padding: 20,
+                    borderRadius: 20
+                  }}>
+                  <Text style={{ color: "white" }}>Completed</Text>
+                </TouchableOpacity>
+              </View>
+
+              {showDetails && <TouchableOpacity
+                style={{
+                  flex: 1,
+                  flexDirection: 'row',
+                  position: 'absolute',
+                  bottom: 80,
+                  alignSelf: "center",
+                  justifyContent: "space-between",
+                  backgroundColor: "white",
+                  borderWidth: 0.5,
+                  paddingHorizontal: 50,
+                  paddingVertical:20,
+                  borderRadius: 20
+                }}>
+                <View>
+                  <Text>Product: {selectedCommand.productId.name}</Text>
+                  <Text>Quantity: {selectedCommand.quantity}</Text>
+                  <Text>Client:{selectedCommand.userId.username}</Text>
+                  <Text>Phone:{selectedCommand.userId.phone}</Text>
+                  <Text>Price:{selectedCommand.productId.price}</Text>
+
+                </View>
+              </TouchableOpacity>}
+            </View>
+          }
         </Modal>
-)}
+      )}
     </View>
   );
 };
